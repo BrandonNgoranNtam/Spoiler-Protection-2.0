9bfda8a6d0a9dc8a3ebd1dbcba2e4c14d0119a36
Brandon Ngoran Ntam <brandonzoom886@gmail.com>
2024-05-06 08:48:21 +0200

    Comments for the spoiler cover

--- app/ui/modules/spoilerCoverBackground/spoilerCoverBackgroundLogic.js
+++ app/ui/modules/spoilerCoverBackground/spoilerCoverBackgroundLogic.js
@@ -1,52 +1,55 @@
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    app.ui.modules.spoilerCoverBackgroundLogic = app.ui.modules.spoilerCoverBackgroundLogic  || (function() {
-        return {
+  // Define the spoilerCoverBackgroundLogic module within the app.ui.modules namespace
+  // If it already exists, it will not be redefined.
 
-            //#region properties and consts
+  app.ui.modules.spoilerCoverBackgroundLogic =
+    app.ui.modules.spoilerCoverBackgroundLogic ||
+    (function () {
+      return {
+        // The name of the module, used for identification
+        moduleName: "spoilerCoverBackground",
+        // Default background color for the spoiler cover
+        defaultBackgroundColor: "#B22222",
 
-            moduleName: "spoilerCoverBackground",
-            defaultBackgroundColor: "#B22222",
-            //#endregion
+        // Initializes the default values for the spoiler cover background settings
+        initSettingsDefaultValues: function () {
+          // Ensure the spoilerCoverBackground setting object exists
+          core.utilities.settings.spoilerCoverBackground = {};
+          // Set the default background color
+          core.utilities.settings.spoilerCoverBackground.backgroundColor =
+            app.ui.modules.spoilerCoverBackgroundLogic.defaultBackgroundColor;
+        },
 
-            //#region init
+        // Extends the core settings with a new function to set the background color
+        extendSetting: function () {
+          // Define a function to update the background color and persist the change
+          core.utilities.settings.setBackgroundColor = function (
+            color,
+            successCallback
+          ) {
+            // Update the background color in the settings
+            core.utilities.settings.spoilerCoverBackground.backgroundColor =
+              color;
+            // Persist the updated settings
+            core.utilities.settings.updateSettingsInStorage(successCallback);
+          };
+        },
 
-            initSettingsDefaultValues: function() {
-                core.utilities.settings.spoilerCoverBackground = {};
-                core.utilities.settings.spoilerCoverBackground.backgroundColor = app.ui.modules.spoilerCoverBackgroundLogic.defaultBackgroundColor;
-            },
-
-            extendSetting: function() {
-                core.utilities.settings.setBackgroundColor = function (color, successCallback) {
-                    core.utilities.settings.spoilerCoverBackground.backgroundColor = color;
-                    core.utilities.settings.updateSettingsInStorage(successCallback);
-                };
-            },
-
-            //#endregion
-
-            //#region public functions
-
-            getBackgroundColor:  function() {
-                return core.utilities.settings.spoilerCoverBackground.backgroundColor;
-            }
-
-            //#endregion
-
-            //#region private functions
-
-            //#endregion
-
-        }
+        // Retrieves the current background color from the settings
+        getBackgroundColor: function () {
+          return core.utilities.settings.spoilerCoverBackground.backgroundColor;
+        },
+      };
     })();
 
-    core.utilities.settings.addSettingModifierModule(app.ui.modules.spoilerCoverBackgroundLogic);
-    core.utilities.settings.addOnExtendSettingsEvent(app.ui.modules.spoilerCoverBackgroundLogic.extendSetting);
-
+  // Register this module as a settings modifier module
+  core.utilities.settings.addSettingModifierModule(
+    app.ui.modules.spoilerCoverBackgroundLogic
+  );
+  // Register the extendSetting function to be called when settings are extended
+  core.utilities.settings.addOnExtendSettingsEvent(
+    app.ui.modules.spoilerCoverBackgroundLogic.extendSetting
+  );
 })();
-
-
-
-
-
